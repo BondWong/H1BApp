648183e2856f38379bcdda7b0353c3c6b34d17ae
JavaEEBoy <WongZeonbong@gmail.com>
2015-12-28 17:44:21 -0500

    Initial Commit

---
+++ H1-B/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  H1-B
+//
+//  Created by Bond Wong on 12/28/15.
+//  Copyright © 2015 Bond Wong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
